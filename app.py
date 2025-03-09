527d70b6f480e78fb921fd61366329a7d1778311
Mohamed Samir <mohamedsamir170569@gmail.com>
2025-03-09 21:42:16 +0200

    app.py modified

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, session, redirect
 from functools import wraps
 from pymongo import MongoClient
+from flask_cors import CORS
 
 # Create Flask app
 app = Flask(__name__)
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
-
+CORS(app,supports_credentials=True)
 # Setup MongoDB
 client = MongoClient("mongodb://localhost:27017/")
 db = client["Cluster1"]
@@ -37,5 +38,5 @@ from product.routes import product_bp
 app.register_blueprint(user_bp)
 app.register_blueprint(product_bp)
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run(debug=True)
